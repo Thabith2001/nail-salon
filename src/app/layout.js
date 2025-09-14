becc13547c0fdee2cf9f56e36a288c3227544713
thabith <thabith20010909@gmail.com>
2025-09-14 13:22:11 +0530

    Merge remote-tracking branch 'origin/main'
    # Conflicts:
    #	src/app/layout.js
    #	src/app/page.js

--- src/app/layout.js
+++ src/app/layout.js
@@ -25,7 +25,7 @@ export default function RootLayout({children}) {
     return (
         <html lang="en">
         <body
-            className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+            className={`${geistSans.variable} ${geistMono.variable} antialiased min-h-screen flex flex-col overflow-x-hidden `}
         >
         <Header/>
         {children}
